3c4305253f031f87002e45fdcffc3206ebba08be
Vencelg <gazdavaclav@gmail.com>
2024-07-01 17:13:46 +0200

    Implement client side search and filtering

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,17 +10,17 @@
         </button>
 
         <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-            <div class="h-full px-3 py-4 overflow-y-auto bg-white bg-opacity-80 backdrop-blur-md dark:bg-gray-800">
+            <div class="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800">
                 <ul class="space-y-2 font-medium">
                     <li class="mb-5">
                         <img src="{{asset('images/logo-no-bg.png')}}" alt="Reservoo logo">
                     </li>
                     <li>
-                        <a href="#" class="flex items-center p-2 rounded-lg dark:text-white hover:text-blue-600 hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="w-5 h-5 transition duration-75 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                        <a href="{{route('home')}}" class="flex items-center p-2 rounded-lg dark:text-white hover:text-blue-600 hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="w-6 h-6 hover:text-blue-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M5.535 7.677c.313-.98.687-2.023.926-2.677H17.46c.253.63.646 1.64.977 2.61.166.487.312.953.416 1.347.11.42.148.675.148.779 0 .18-.032.355-.09.515-.06.161-.144.3-.243.412-.1.111-.21.192-.324.245a.809.809 0 0 1-.686 0 1.004 1.004 0 0 1-.324-.245c-.1-.112-.183-.25-.242-.412a1.473 1.473 0 0 1-.091-.515 1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.401 1.401 0 0 1 13 9.736a1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.4 1.4 0 0 1 9 9.74v-.008a1 1 0 0 0-2 .003v.008a1.504 1.504 0 0 1-.18.712 1.22 1.22 0 0 1-.146.209l-.007.007a1.01 1.01 0 0 1-.325.248.82.82 0 0 1-.316.08.973.973 0 0 1-.563-.256 1.224 1.224 0 0 1-.102-.103A1.518 1.518 0 0 1 5 9.724v-.006a2.543 2.543 0 0 1 .029-.207c.024-.132.06-.296.11-.49.098-.385.237-.85.395-1.344ZM4 12.112a3.521 3.521 0 0 1-1-2.376c0-.349.098-.8.202-1.208.112-.441.264-.95.428-1.46.327-1.024.715-2.104.958-2.767A1.985 1.985 0 0 1 6.456 3h11.01c.803 0 1.539.481 1.844 1.243.258.641.67 1.697 1.019 2.72a22.3 22.3 0 0 1 .457 1.487c.114.433.214.903.214 1.286 0 .412-.072.821-.214 1.207A3.288 3.288 0 0 1 20 12.16V19a2 2 0 0 1-2 2h-6a1 1 0 0 1-1-1v-4H8v4a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2v-6.888ZM13 15a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2Z" clip-rule="evenodd"/>
                             </svg>
+
                             <span class="ms-3">Restaurants</span>
                         </a>
                     </li>
@@ -47,7 +47,7 @@
             </div>
         </aside>
 
-        <div class="p-4 sm:ml-64 backdrop-blur-2xl">
+        <div class="sm:ml-64 overflow-scroll h-screen backdrop-blur-2xl">
             @yield('main-content')
         </div>
     </main>
--- resources/views/main/restaurant/homepage.blade.php
+++ resources/views/main/restaurant/homepage.blade.php
@@ -1,23 +1,48 @@
 @extends('layout.main')
 
 @section('main-content')
-    <div class="w-[80%] m-auto">
-        <form action="" class="w-full bg-blue-600 p-5 rounded-2xl mb-5">
-            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
+    <div class="pt-4 w-[80%] m-auto">
+        <div class="w-full bg-blue-600 p-5 rounded-2xl mb-5 flex justify-between items-end">
+            <div class="flex w-2/3 mr-5">
+                <div class="w-full">
+                        <label for="search" class="block mb-2 text-sm font-medium text-white dark:text-white">Search restaurants</label>
+                    <div class="relative w-full">
+                        <input onvolumechange="applySearch()" type="search" id="search" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="What are you looking for..." required />
+                    </div>
                 </div>
-                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What would you like today?" required />
-                <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
-        </form>
-        <div class="grid grid-cols-3 gap-5 justify-items-center">
+
+            <div class="flex justify-between w-1/3">
+                <div>
+                    <label for="cuisine" class="block mb-2 text-sm font-medium text-white dark:text-white">Select a cuisine</label>
+                    <select id="cuisine" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="" selected>All cuisines</option>
+                        @foreach($tags as $tag)
+                            <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div>
+                    <label for="sort" class="block mb-2 text-sm font-medium text-white dark:text-white">Sort by</label>
+                    <select id="sort" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="name" selected>Name</option>
+                        <option value="highest_rating">Highest Rating</option>
+                        <option value="lowest_rating">Lowest Rating</option>
+                        <option value="most_reviews">Most Reviews</option>
+                        <option value="least_reviews">Least Reviews</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div id="restaurantList" class="grid grid-cols-3 gap-5 justify-items-center">
+            <div id="not_found_block" style="display: none" class="col-span-3 w-full bg-blue-600 text-white rounded-2xl text-center p-4">
+                <svg class="w-1/12 m-auto fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M175.9 448c-35-.1-65.5-22.6-76-54.6C67.6 356.8 48 308.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208s-93.1 208-208 208c-28.4 0-55.5-5.7-80.1-16zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM128 369c0 26 21.5 47 48 47s48-21 48-47c0-20-28.4-60.4-41.6-77.7c-3.2-4.4-9.6-4.4-12.8 0C156.6 308.6 128 349 128 369zm128-65c-13.3 0-24 10.7-24 24s10.7 24 24 24c30.7 0 58.7 11.5 80 30.6c9.9 8.8 25 8 33.9-1.9s8-25-1.9-33.9C338.3 320.2 299 304 256 304zm47.6-96a32 32 0 1 0 64 0 32 32 0 1 0 -64 0zm-128 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>
+                <h1>Sorry, we could't find what you were looking for...</h1>
+            </div>
             @if($restaurants)
                 @foreach($restaurants as $restaurant)
-                    <div class="transition-all duration-[90ms] hover:outline outline-blue-600 outline-offset-2 outline-4 w-full bg-white rounded-2xl shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div data-name="{{$restaurant->name}}" data-rating="{{$restaurant->rating}}" data-reviews="{{$restaurant->reviews}}" class="restaurant_object transition-all duration-[90ms] hover:outline outline-blue-600 outline-offset-2 outline-4 w-full bg-white rounded-2xl shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="{{route('restaurant.detail', ['id' => $restaurant->id])}}">
                             <div style="background-image: url({{$restaurant->banner_url}});" class="h-[10rem] bg-cover bg-no-repeat rounded-t-2xl"></div>
                         </a>
@@ -35,13 +60,101 @@
                                     <a class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">{{$restaurant->reviews}} reviews</a>
                                 </div>
                             </div>
-                            @foreach($restaurant->tags as $tag)
-                                <span style="background-color: {{$tag->color}}" class="text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{{$tag->name}}</span>
-                            @endforeach
+                            <div id="tagList">
+                                @foreach($restaurant->tags as $tag)
+                                    <span data-id="{{$tag->id}}" style="background-color: {{$tag->color}}" class="tag_object text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{{$tag->name}}</span>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 @endforeach
             @endif
         </div>
     </div>
+
+    <script>
+        const search = document.getElementById('search');
+        const sort = document.getElementById('sort');
+        const cuisine = document.getElementById('cuisine');
+
+        search.addEventListener('input', () => {
+            applySearchAndCuisine(search.value.toLowerCase(), cuisine.value);
+        });
+
+        sort.addEventListener('change', () => {
+            applySort(sort.value);
+        });
+
+        cuisine.addEventListener('change', () => {
+            applySearchAndCuisine(search.value.toLowerCase(), cuisine.value);
+        });
+
+        function applySearchAndCuisine(search, selectedTag) {
+            const restaurants = Array.from(document.querySelectorAll('#restaurantList .restaurant_object'));
+
+            restaurants.forEach(function(restaurant) {
+                const name = restaurant.getAttribute('data-name').toLowerCase();
+                const tags = Array.from(restaurant.querySelectorAll('#tagList .tag_object')).map(tag => tag.getAttribute('data-id'));
+
+                const matchesTag = !selectedTag || tags.includes(selectedTag);
+                const matchesSearch = name.includes(search);
+
+                if (matchesSearch && matchesTag) {
+                    restaurant.style.display = '';
+                } else {
+                    restaurant.style.display = 'none';
+                }
+            });
+
+            const notFoundBlock = document.getElementById('not_found_block');
+            const visibleRestaurants = restaurants.filter(function (restaurant) {
+                return restaurant.style.display !== 'none';
+            });
+
+            if (visibleRestaurants.length === 0) {
+                notFoundBlock.style.display = '';
+            }else {
+                notFoundBlock.style.display = 'none';
+            }
+        }
+
+        function applySort(sort) {
+            const restaurants = Array.from(document.querySelectorAll('#restaurantList .restaurant_object'));
+
+            restaurants.sort(function (a, b) {
+                switch (sort) {
+                    case 'most_reviews':
+                        return b.getAttribute('data-reviews') - a.getAttribute('data-reviews');
+                    case 'highest_rating':
+                        return b.getAttribute('data-rating') - a.getAttribute('data-rating');
+                    case 'least_reviews':
+                        return a.getAttribute('data-reviews') - b.getAttribute('data-reviews');
+                    case 'lowest_rating':
+                        return a.getAttribute('data-rating') - b.getAttribute('data-rating');
+                    default:
+                        return a.getAttribute('data-name').localeCompare(b.getAttribute('data-name'));
+                }
+            });
+
+            const restaurantList = document.getElementById('restaurantList');
+            restaurants.forEach(function (restaurant) {
+                restaurantList.appendChild(restaurant);
+            });
+        }
+
+        function showNotFound() {
+            const notFondBlock = document.getElementById('#not_found_block');
+            const restaurants = Array.from(document.querySelectorAll('#restaurantList .restaurant_object'));
+            const visibleRestaurants = restaurants.filter(function (restaurant) {
+                return restaurant.style.display !== 'none';
+            });
+            console.log(notFondBlock)
+
+            if (visibleRestaurants.length === 0) {
+                notFondBlock.style.display = '';
+            }else {
+                notFondBlock.style.display = 'none';
+            }
+        }
+    </script>
 @endsection
