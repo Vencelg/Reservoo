1ee507754670c8638072739bf058b4ed39ccffcc
Vencelg <gazdavaclav@gmail.com>
2024-07-06 01:47:09 +0200

    Add review deletion

--- resources/views/main/reviews/list.blade.php
+++ resources/views/main/reviews/list.blade.php
@@ -98,6 +98,20 @@
                                     <p class="text-base font-semibold text-gray-900 dark:text-white">{{$review->user->name}}</p>
                                     <p class="text-sm font-normal text-gray-500 dark:text-gray-400">{{$review->created_at}}</p>
                                 </div>
+
+                                @if(Auth::id() === $review->user->id)
+                                    <div>
+                                        <form action="{{route('reviews.destroy', ['id' => $review->id])}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">
+                                                <svg class="w-6 h-6 text-gray-800 hover:text-red-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+                                                </svg>
+                                            </button>
+                                        </form>
+                                    </div>
+                                @endif
                             </div>
 
                             <div class="mt-4 min-w-0 flex-1 space-y-4 sm:mt-0">
@@ -112,6 +126,10 @@
 
     <script>
         const sort = document.getElementById('sort');
+
+        window.addEventListener('load', () => {
+            applySort(sort.value);
+        });
 
         sort.addEventListener('change', () => {
             applySort(sort.value);
